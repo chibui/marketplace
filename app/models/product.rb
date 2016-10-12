8842f6d2d223be531d7b9d4841861361c8f2ce84
chi <chi_bui@outlook.com>
2016-10-12 11:35:00 +1100

    add store views and caching

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+  validates :title, :description, :origin, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+
+  # returns most recently updated product
+  def self.latest
+    Product.order(:updated_at).last
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'store/index'
+
   resources :products
   root 'pages#landing'
 
